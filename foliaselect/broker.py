abe1f43923751f3a41150b9532381085d3324173
ErwinKomen <E.Komen@ru.nl>
2016-12-21 16:25:30 +0100

    Added 'resume' option and included downloading of FoLiA files

--- foliaselect/broker.py
+++ foliaselect/broker.py
@@ -17,6 +17,7 @@ except ImportError:
 
 # ----------------------------------------------------------------------------------
 NEDERLAB_BROKER = "http://www.nederlab.nl/broker2/search/"
+NEDERLAB_OPENSKOS = "https://openskos.meertens.knaw.nl/nederlab/archief/get/"
 
 # ----------------------------------------------------------------------------------
 # Name :    foliareq
@@ -40,7 +41,7 @@ class broker:
     # Name :    task2request
     # Goal :    Convert one task object into a broker request
     #           Task object: 
-    #             {"genre": "verhalen", "year": 1990, "number": 50}
+    #             {"genre": "verhalen", "start": 1990, "end": 1995, "number": 50}
     # History:
     # 20/dec/2016    ERK Created
     # ----------------------------------------------------------------------------------
@@ -48,30 +49,45 @@ class broker:
         """Convert a broker task to a request"""
 
         # Validate data we receive
-        if oTask == None or not 'year' in oTask or not 'genre' in oTask or not 'number' in oTask:
+        if oTask == None or not 'start' in oTask or not 'genre' in oTask or not 'number' in oTask:
             # Return the None object
             return None
 
+        # Figure out start and end
+        iStart = oTask["start"]
+        if "end" in oTask:
+            iEnd = oTask["end"]
+        else:
+            iEnd = iStart + 1
+
         # Create a correct object
-        oData = { 'condition': {
-                    'type': 'and',
-                    'list': [
-                        { 'type': 'equals', 'field': 'NLCore_NLAdministrative_sourceCollection', 'value': 'DBNL' },
-                        { 'type': 'equals', 'field': 'NLTitle_genre', 'value': oTask['genre']},
-                        { 'type': 'range', 'field': 'NLTitle_yearOfPublicationMin',
-                            'start': oTask['year'], 'end': oTask['year']+1 }
+        oData = { "condition": {
+                    "type": "and",
+                    "list": [
+                        { "type": "cql", "field": "NLContent_mtas", "value": "[]" },
+                        { "type": "equals", "field": "NLCore_NLAdministrative_sourceCollection", "value": "DBNL" },
+                        { "type": "equals", "field": "NLTitle_genre", "value": oTask["genre"]},
+                        { "type": "range", "field": "NLTitle_yearOfPublicationMin",
+                            "start": str(iStart), "end": str(iEnd) }
                       ]
                   },
-                  'response': {
-                    'documents': oTask['number'], 'start': 0,
-                    'fields': [
-                        "NLCore_NLIdentification_nederlabID",
-                        "NLCore_NLAdministrative_sourceCollection",
-                        "NLTitle_title", "NLTitle_yearOfPublicationMin",
-                        "NLTitle_yearOfPublicationMax",
-                        "NLTitle_yearOfPublicationLabel",
-                        "NLTitle_genre"
-                    ]}
+                  "response": {
+                    "documents": {
+                        "number": oTask["number"], "start": 0,
+                        "fields": [
+                            "NLCore_NLIdentification_nederlabID",
+                            "NLCore_NLAdministrative_sourceCollection",
+                            "NLTitle_title", "NLTitle_yearOfPublicationMin",
+                            "NLTitle_yearOfPublicationMax",
+                            "NLTitle_yearOfPublicationLabel",
+                            "NLTitle_genre",
+                            {   "name": "title_authorinfo",
+                                "join": {"from":"NLTitle_NLPersonRef_personID", "to": "NLCore_NLIdentification_nederlabID"},
+                                "fields": ["NLPerson_NLPersonName_preferredFullName"]
+                            }
+                        ]},
+                    "stats": True
+                    }
                 }
         # Return the object that we have made
         return oData
@@ -86,7 +102,9 @@ class broker:
     def request(self, oData):
         """ Issue a request to the broker"""
 
-        data = urllib.parse.urlencode(oData).encode('ascii')
+        # data = urllib.parse.urlencode(oData).encode('ascii')
+        # data = str(oData).replace("'", '"').encode('ascii')
+        data = json.dumps(oData).encode('ascii')
         strUri = NEDERLAB_BROKER      
         oPost = {'Accept':'application/json', 
                'Content-Type': 'application/x-www-form-urlencoded'}
@@ -115,3 +133,34 @@ class broker:
        
         # Getting here means that we have a valid [oResult] object
         return oResult
+
+    def getFolia(self, sFoliaId, flOutput):
+        """Retrieve the folia file and save it"""
+
+        strUri = NEDERLAB_OPENSKOS + sFoliaId
+        # Prepare a request
+        req = urllib.request.Request(strUri, method='GET')
+
+        # Make sure the output is GZ
+        if not flOutput.endswith(".gz"):
+            flOutput += ".gz"
+
+        try:
+            with urllib.request.urlopen(req, timeout=20) as response:
+                # Read the response
+                with open(flOutput,"wb") as fOut:
+                    fOut.write(response.read())
+
+            # Return positively
+            return True
+        except urllib.error.URLError as e:
+            # Show the user what is wrong
+            self.errHandle.Status('URLopen URL error: {}\nURI: {}\n'.format(
+                e.reason, strUri))
+            # Return failure
+            return False
+        except:
+            # Show the user what is wrong
+            self.errHandle.DoError("Could not retrieve a file")
+            # Return failure
+            return False
--- foliaselect/foliaselect.py
+++ foliaselect/foliaselect.py
@@ -22,6 +22,7 @@ def main(prgName, argv) :
     flInput = ''        # input file name
     flOutput = ''       # output file name
     flStat = ''         # statistics file name
+    sMethod = ''        # Method to be used
 
     try:
         # Adapt the program name to exclude the directory
@@ -32,7 +33,7 @@ def main(prgName, argv) :
         # get all the arguments
         try:
             # Get arguments and options
-            opts, args = getopt.getopt(argv, "hs:i:o:", ["-statfile=","-inputfile=","-outputfile="])
+            opts, args = getopt.getopt(argv, "hs:i:o:m:", ["-statfile=","-inputfile=","-outputfile=", "-method="])
         except getopt.GetoptError:
               print(sSyntax)
               sys.exit(2)
@@ -47,6 +48,8 @@ def main(prgName, argv) :
                 flInput = arg
             elif opt in ("-o", "--ofile"):
                 flOutput = arg
+            elif opt in ("-m", "--method"):
+                sMethod = arg
         # Check if all arguments are there
         if (flInput == '' or flOutput == '' or flStat == ''):
             errHandle.DoError(sSyntax)
@@ -55,7 +58,10 @@ def main(prgName, argv) :
         errHandle.Status('Output is "' + flOutput + '"')
         errHandle.Status('Statistics: "' + flStat + '"')
         # Call the function that converst input into output
-        if (foliaselect(flInput, flOutput, flStat)) :
+        kwargs = {"input": flInput, "output": flOutput, "stat": flStat}
+        if sMethod != '':
+            kwargs['method'] = sMethod
+        if (foliaselect(**kwargs)) :
             errHandle.Status("Ready")
         else :
             errHandle.DoError("Could not complete")    
@@ -70,9 +76,17 @@ def main(prgName, argv) :
 # History:
 # 20/dec/2016    ERK Created
 # ----------------------------------------------------------------------------------
-def foliaselect(flInput, flOutput, flStat):
+def foliaselect(**kwargs):
 
     try:
+        # Check
+        if not "input" in kwargs or not "output" in kwargs: return False
+        # Get the obligatory parameters from the kwargs
+        flInput = kwargs['input']
+        flOutput = kwargs['output']
+        sMethod = ''
+        if "method" in kwargs: sMethod = kwargs['method']
+
         # Start a broker communication instance
         oBroker = broker.broker(errHandle)
 
@@ -85,6 +99,9 @@ def foliaselect(flInput, flOutput, flStat):
         with open(flInput, "r") as fIn:
             oInput = json.load(fIn)
         sDate = oInput['date']
+        iTotal = 0
+        # Derive an output directory
+        sDirOut = os.path.dirname(flOutput)
         # Load the collection array
         lstCollection = oInput['collection']
         # Walk the collections
@@ -93,7 +110,7 @@ def foliaselect(flInput, flOutput, flStat):
             sTitle = oCol['title']
             lstGather = oCol['gather']
             # Walk all gather elements
-            for oGather in lstGather:
+            for iGather, oGather in enumerate(lstGather):
                 # Get the information from this object
                 oRequest = oBroker.task2request(oGather)
                 oResponse = oBroker.request(oRequest)
@@ -101,6 +118,53 @@ def foliaselect(flInput, flOutput, flStat):
                 if oResponse == None:
                     errHandle.DoError("Could not get a response from the broker")
                     return False
+                elif oResponse['status'] != 'ok':
+                    errHandle.DoError("Broker returned error: " + oResponse['status'])
+                    return False
+
+                # Process the documents we received
+                iResults = oResponse['stats']['total']
+                oGather['results'] = iResults
+                oGather['documents'] = oResponse['documents']
+                # Keep track of how much we get
+                iTotal += iResults
+                # Show what we are doing
+                if "end" in oGather:
+                    iEnd = oGather['end']
+                else:
+                    iEnd = oGather['start'] + 1
+                errHandle.Status("{} collection[{}] genre[{}] years: [{}-{}]: {}".format(
+                    sDate, sTitle, oGather['genre'], oGather['start'], iEnd, iResults))
+
+                # Create a directory for these results
+                sResDir = os.path.join(os.path.abspath(sDirOut), oCol['dir'])
+                if not os.path.exists(sResDir): os.mkdir(sResDir)
+                sResDir = os.path.join(os.path.abspath(sResDir), str(iGather) )
+                if not os.path.exists(sResDir): os.mkdir(sResDir)
+
+                # Walk through the results
+                for iRes, oRes in enumerate(oGather['documents']):
+                    # Create a file name
+                    sResBase = os.path.join(sResDir, str(iRes) )
+                    sResFolia = sResBase + ".folia.xml"
+                    sResJson = sResBase + ".json"
+
+                    # Save the meta information
+                    with open(sResJson, "w") as fMeta:
+                        json.dump(oRes, fMeta, indent=2)
+
+                    # Resumption?
+                    if sMethod != 'resume' or not os.path.exists(sResFolia + ".gz"):
+                        # Get and save the folia file
+                        print("Downloading: {}".format(sResFolia))
+                        oBroker.getFolia(oRes['NLCore_NLIdentification_nederlabID'], sResFolia)
+
+        # Save all the document details
+        errHandle.Status("saving to: " + flOutput)
+        with open(flOutput, "w") as fOut:
+            json.dump(oInput, fOut, indent=2)
+
+        errHandle.Status("Results: {}".format(iTotal))
 
         # Return positively
         return True
